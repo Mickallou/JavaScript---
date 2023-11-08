f7a2a7a0dc896b31a369d0cf3b24b74edd06c118
Mickael Allouche <allouchemicka@gmail.com>
2023-11-08 21:35:51 +0200

    8/11/2023

---
+++ 11.01.2023/script.js
@@ -0,0 +1,30 @@
+const obj = {
+    name: "Mickael",
+    address: {
+        city: "Ashdod",
+    }
+};
+console.log(obj.name); //Mickael                                           
+console.log(obj.address.city); //Ashdod  
+/**
+ * Escape Characters (\ in a string)
+ * 
+ * for \ write \\
+ * for new line write \r\n
+ * for ' in a string \'
+ * for "" in a string \"
+ * for tab character write \t
+ * 
+ */
+console.log("Hi my name is \"Mickael\" 'Allouche' 04\\10\\1998");
+console.log("Dog\'s");
+console.log("Hello\tMy name is");
+console.log("Hello\r\nYOU");
+
+/**
+ * number
+ */
+console.log(9);
+console.log(-4);
+console.log(-87.9);
+console.log(1_000_000.5);
---
+++ 11.08.2023/script.js
@@ -0,0 +1,20 @@
+console.log({
+    "Hi how are you?": {
+        name: "Mickael",
+        email: "mail@gmail.com",
+        date: new Date() + "\\",
+    },
+});
+
+
+let a = {
+    toString: function () {
+        return ("Mickael");
+    },
+    b: 25,
+};
+
+console.log(String(a))
+console.log(a.toString())
+
+console.log("\\" + "dsfdsfsdf")
